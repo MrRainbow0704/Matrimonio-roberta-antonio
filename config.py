65dfe15e25109f65a60b71cedc008d0dc7f36faa
MrRainbow0704 <2007.marcosimone@gmail.com>
2024-03-24 17:20:42 +0100

    Added docker support Added gunicorn

--- config.py
+++ config.py
@@ -5,19 +5,20 @@ from pathlib import Path
 
 ROOT_DIR = Path(__file__).resolve().parent
 
-if load_dotenv(dotenv_path=ROOT_DIR / ".env"):
-    # Generale
-    HOST = getenv("HOST")
-    PORT = int(getenv("PORT"))
-    DEBUG = bool(getenv("DEBUG"))
+if not load_dotenv(dotenv_path=ROOT_DIR / ".env"):
+    print("Impossibile caricare un file .env.")
+# Generale
+HOST = getenv("HOST", "0.0.0.0")
+PORT = int(getenv("PORT", "80"))
+DEBUG = bool(getenv("DEBUG", "0"))
 
-    # Admin
-    ADMIN_USERNAME = getenv("ADMIN_USERNAME")
-    ADMIN_PASSWD = getenv("ADMIN_PASSWD")
+# Admin
+ADMIN_USERNAME = getenv("ADMIN_USERNAME", "admin")
+ADMIN_PASSWD = getenv("ADMIN_PASSWD", "admin")
 
-    # Database
-    DB_HOST = getenv("DB_HOST")
-    DB_PORT = int(getenv("DB_PORT"))
-    DB_USER = getenv("DB_USER")
-    DB_PASSWD = getenv("DB_PASSWD")
-    DB_NAME = getenv("DB_NAME")
+# Database
+DB_HOST = getenv("DB_HOST", "127.0.0.1")
+DB_PORT = int(getenv("DB_PORT", "3306"))
+DB_USER = getenv("DB_USER", "root")
+DB_PASSWD = getenv("DB_PASSWD", None)
+DB_NAME = getenv("DB_NAME", "database")
