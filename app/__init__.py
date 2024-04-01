6c7c0203425eaed2d20b9b5b8c9339d24997c7e5
MrRainbow0704 <2007.marcosimone@gmail.com>
2024-04-01 23:46:35 +0200

    Aggiunta pagina "programma". Leggere modifiche estetiche

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,10 @@
 from flask import Flask
 import config
-from werkzeug.middleware.profiler import ProfilerMiddleware
 from . import tools
 
 
 app = Flask(__name__, root_path=config.ROOT_DIR)
 app.secret_key = config.SECRET_KEY
-app.wsgi_app = ProfilerMiddleware(
-    app.wsgi_app, profile_dir=str(config.ROOT_DIR / "profiler")
-)
 
 DB = {
     "host": config.DB_HOST,
