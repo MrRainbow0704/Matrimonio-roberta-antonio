9a3c86cb92912f92b05f14471b9b6aa0d2b2750a
MrRainbow0704 <2007.marcosimone@gmail.com>
2024-04-28 23:18:22 +0200

    Risolto errore per l'orm in cui il valore default non funzionava

--- app/tools/orm/invitato.py
+++ app/tools/orm/invitato.py
@@ -1,6 +1,6 @@
-from sqlalchemy import Integer, String, Boolean, Text
+from sqlalchemy import Integer, String, Boolean, Text, text
 from sqlalchemy.orm import mapped_column, Mapped
-from typing import Dict
+from typing import Optional
 from . import Base
 
 allergie = '{"anidride-solforosa": 0, "arachidi": 0, "crostacei": 0, "frutta-a-guscio": 0, "glutine": 0, "latte": 0, "lupini": 0, "molluschi": 0, "pesce": 0, "sedano": 0, "senape": 0, "sesamo": 0, "soia": 0, "uova": 0}'
@@ -15,10 +15,10 @@ class Invitato(Base):
     nome: Mapped[str] = mapped_column(String(32), nullable=False)
     cognome: Mapped[str] = mapped_column(String(32), nullable=False)
     famiglia: Mapped[int] = mapped_column(Integer, nullable=False)
-    partecipa: Mapped[bool] = mapped_column(Boolean, nullable=False, default=0)
-    allergie: Mapped[str] = mapped_column(Text, nullable=False, default=allergie)
-    tipo: Mapped[str] = mapped_column(String(8), nullable=False, default="adulto")
-    età: Mapped[str] = mapped_column(String(4), nullable=True)
+    partecipa: Mapped[bool] = mapped_column(Boolean, nullable=False, server_default=text("0"))
+    allergie: Mapped[str] = mapped_column(Text, nullable=False, server_default=allergie)
+    tipo: Mapped[str] = mapped_column(String(8), nullable=False,server_default="adulto")
+    età: Optional[Mapped[str]] = mapped_column(String(4), nullable=True)
 
     def __repr__(self) -> str:
         return (
